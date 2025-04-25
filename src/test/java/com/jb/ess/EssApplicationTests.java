0c2bfd6589442c1247933707c5627dcd074457b4
lastcoder <2019301032@skuniv.ac.kr>
2025-04-25 14:42:34 +0900

    login

---
+++ src/test/java/com/jb/ess/EssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jb.ess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
