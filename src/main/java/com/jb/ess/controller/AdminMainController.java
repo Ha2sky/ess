298391114364a29817bef1b4086519c5f9eba320
lastcoder <2019301032@skuniv.ac.kr>
2025-04-28 11:32:01 +0900

    classify htmls

--- src/main/java/com/jb/ess/controller/AdminMainController.java
+++ src/main/java/com/jb/ess/controller/AdminMainController.java
@@ -4,9 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class PatternController {
+public class AdminMainController {
+    @GetMapping("/admin/deptmanage")
+    public String deptManagementPage() {
+        return "admin/deptmanage";
+    }
+
     @GetMapping("/admin/pattern")
     public String patternPage() {
-        return "pattern";
+        return "admin/pattern";
     }
 }
