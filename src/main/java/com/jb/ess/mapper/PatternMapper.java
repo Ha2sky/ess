a404cf2a3c6c8c81d483053bf27553b8eef9b9aa
Ha2sky <qkrwhdrjs13@gmail.com>
2025-05-14 13:54:21 +0900

    근태패턴 생성(로직x)

--- src/main/java/com/jb/ess/mapper/PatternMapper.java
+++ src/main/java/com/jb/ess/mapper/PatternMapper.java
@@ -2,6 +2,7 @@ package com.jb.ess.mapper;
 
 import com.jb.ess.domain.PatternDetail;
 import java.util.List;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -11,5 +12,33 @@ public interface PatternMapper {
         SELECT *
         FROM HRTSHIFTPATTERNDTL
     """)
-    List<PatternDetail> findAllPatterns(); // 실제론 사용자별 패턴 필요
+    /* 모든 근태패턴 조회 */
+    List<PatternDetail> findAllPatterns();
+
+    @Insert("""
+        INSERT INTO HRTSHIFTPATTERNDTL (
+            WORK_PATTERN_CODE,
+            WORK_PATTERN_NAME,
+            MON_SHIFT_CODE,
+            TUE_SHIFT_CODE,
+            WED_SHIFT_CODE,
+            THU_SHIFT_CODE,
+            FRI_SHIFT_CODE,
+            SAT_SHIFT_CODE,
+            SUN_SHIFT_CODE
+        )
+        VALUES (
+            #{workPatternCode},
+            #{workPatternName},
+            #{monShiftCode},
+            #{tueShiftCode},
+            #{wedShiftCode},
+            #{thuShiftCode},
+            #{friShiftCode},
+            #{satShiftCode},
+            #{sunShiftCode}
+        )
+    """)
+    /* 근태패턴 저장 */
+    void insertShiftPattern(PatternDetail pattern);
 }
--- src/main/java/com/jb/ess/util/DateUtil.java
+++ src/main/java/com/jb/ess/util/DateUtil.java
@@ -1,7 +1,12 @@
 package com.jb.ess.util;
 
 import java.time.LocalDate;
+import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
+import java.time.format.TextStyle;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class DateUtil {
     private DateUtil() {}
@@ -9,7 +14,7 @@ public class DateUtil {
     private static final DateTimeFormatter INPUT_FORMAT = DateTimeFormatter.ofPattern("yyyyMMdd");
     private static final DateTimeFormatter OUTPUT_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
-    // yyyyMMdd → yyyy-MM-dd
+    /* yyyyMMdd → yyyy-MM-dd */
     public static String formatDate(String dateStr) {
         if (dateStr == null || dateStr.isBlank()) return "-";
         if (MAX_DATE.equals(dateStr)) return "-";
@@ -20,5 +25,19 @@ public class DateUtil {
         } catch (Exception e) {
             return "";
         }
+    }
+
+    /* 1일 2일 -> 1일(월) 2일(화) */
+    public static List<String> getDateHeaders(YearMonth month) {
+        List<String> headers = new ArrayList<>();
+        int daysInMonth = month.lengthOfMonth();
+
+        for (int day = 1; day <= daysInMonth; day++) {
+            LocalDate date = month.atDay(day);
+            String dayOfWeek = date.getDayOfWeek().getDisplayName(TextStyle.SHORT, Locale.KOREAN); // 월, 화, 수
+            headers.add(day + "일(" + dayOfWeek + ")");
+        }
+
+        return headers;
     }
 }
