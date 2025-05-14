b3e74196813d3e02a6e15f3aeca5c0def6af1506
jhkim1220 <rlawhdgk999@gmail.com>
2025-05-14 15:28:07 +0900

    근태 테이블 수정 domain 수정

--- src/main/java/com/jb/ess/domain/Department.java
+++ src/main/java/com/jb/ess/domain/Department.java
@@ -4,14 +4,14 @@ import lombok.Data;
 
 @Data
 public class Department {
-    private String deptCode;    // 부서코드
-    private String deptName;    // 부서명
-    private String parentDept;  // 상위부서
-    private String deptLeader;  // 부서장
-    private String deptCategory;// 부서구분
-    private String startDate;   // 시작일자
-    private String endDate;     // 종료일자
-    private String useYn;       // 사용여부
-
+    private String deptCode;        // 부서코드
+    private String deptName;        // 부서명
+    private String parentDept;      // 상위부서
+    private String deptLeader;      // 부서장
+    private String deptCategory;    // 부서구분
+    private String startDate;       // 시작일자
+    private String endDate;         // 종료일자
+    private String useYn;           // 사용여부
+    private String workPatternCode; // 근태패턴코드
     private int empCount;
 }
