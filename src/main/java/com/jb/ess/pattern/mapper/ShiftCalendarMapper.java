40220aa2cdcd848e227c51242968324ad3b3969a
Ha2sky <qkrwhdrjs13@gmail.com>
2025-05-19 13:39:19 +0900

    근태패턴 삭제

--- src/main/java/com/jb/ess/pattern/mapper/ShiftCalendarMapper.java
+++ src/main/java/com/jb/ess/pattern/mapper/ShiftCalendarMapper.java
@@ -1,6 +1,7 @@
 package com.jb.ess.pattern.mapper;
 
 import com.jb.ess.common.domain.ShiftCalendar;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 
@@ -12,4 +13,12 @@ public interface ShiftCalendarMapper {
     """)
     /* 캘린더 생성 */
     void insertShiftCalendar(ShiftCalendar shiftCalendar);
+
+    @Delete("""
+        DELETE
+        FROM HRTSHIFTCALENDAR
+        WHERE WORK_PATTERN_CODE = #{workPatternCode}
+    """)
+    /* 근태패턴 삭제 */
+    void deleteShiftCalendar(String workPatternCode);
 }
