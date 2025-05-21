5c94016864f17568f8bc0f031a5574c2bf30f83d
jhkim1220 <rlawhdgk999@gmail.com>
2025-05-21 14:19:27 +0900

    근태 테이블 수정 domain 추가, 수정

--- src/main/java/com/jb/ess/common/domain/ShiftPatternDtl.java
+++ src/main/java/com/jb/ess/common/domain/ShiftPatternDtl.java
@@ -8,4 +8,5 @@ public class ShiftPatternDtl {
     private String workPatternCode; // 근태패턴코드
     private String shiftCode;       // 근태코드
     private int dayOfWeek;          // 요일 (1: 월, 2: 화 ... 7: 금)
+    private int totalWorkingHours;  // 총근무시간
 }
